3db0559d31b4690e1dbb873bbadeb69ea9ee0849
gowthamds467-svg <gowthamds467@gmail.com>
2026-09-01 18:57:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import numpy as np
+import joblib
+model = joblib.load("models/fraud_model.pkl")
+scaler = joblib.load("models/scaler.pkl")
+transaction_id = st.text_input("Transaction ID")
+
+time = st.number_input(
+    "Time",
+    min_value=0.0,
+    value=100.0
+)
+
+amount = st.number_input(
+    "Amount",
+    min_value=0.0,
+    value=100.0
+)
+
+feature1 = st.number_input(
+    "Feature 1",
+    value=0.0
+)
+
+feature2 = st.number_input(
+    "Feature 2",
+    value=0.0
+)
+
+if st.button("🔍 Check Transaction"):
+
+    if transaction_id == "":
+        st.warning("Please enter Transaction ID")
+
+    else:
+
+        input_data = np.array([
+            [time, amount, feature1, feature2]
+        ])
+
+        if scaler is not None:
+            input_scaled = scaler.transform(input_data)
+        else:
+            input_scaled = input_data
+
+        prediction = model.predict(input_scaled)[0]
+
+        if hasattr(model, "predict_proba"):
+            probability = model.predict_proba(input_scaled)[0][1]
+        else:
+            probability = 0
+
+        if prediction == 1:
+
+            st.error("🚨 FRAUD TRANSACTION DETECTED")
+
+            st.metric(
+                "Fraud Probability",
+                f"{probability * 100:.2f}%"
+            )
+
+        else:
+
+            st.success("✅ LEGITIMATE TRANSACTION")
+
+            st.metric(
+                "Fraud Probability",
+                f"{probability * 100:.2f}%"
+            )
